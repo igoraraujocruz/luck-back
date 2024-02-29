a536e7bcb60945200d7d74a2f698ad25b792e5ee
Igor <igoraraujocruzz@gmail.com>
2024-02-29 19:07:58 -0300

    route off

--- src/modules/products/infra/routes.ts
+++ src/modules/products/infra/routes.ts
@@ -11,19 +11,24 @@ router.get('/', celebrate({
     },
 }), controller.get)
 
-router.post(
-    '/',
-    celebrate({
-        [Segments.BODY]: {
-            name: Joi.string().required(),
-            price: Joi.number().required(),
-            imgSrc: Joi.string().required(),
-            videoSrc: Joi.string().required(),
-            description: Joi.string().required(),
-            luckDay: Joi.date().required(),
-            quantidadeDeRifas: Joi.number().required(),
-        },
-    }),
-    controller.create,
-);
+
+if(process.env.ROUTE_OFF !== 'true') {
+    router.post(
+        '/',
+        celebrate({
+            [Segments.BODY]: {
+                name: Joi.string().required(),
+                price: Joi.number().required(),
+                imgSrc: Joi.string().required(),
+                videoSrc: Joi.string().required(),
+                description: Joi.string().required(),
+                luckDay: Joi.date().required(),
+                quantidadeDeRifas: Joi.number().required(),
+            },
+        }),
+        controller.create,
+    );
+    
+}
+
 
